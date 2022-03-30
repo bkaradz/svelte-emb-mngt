e15a3dad3e33311944d0022ae7ce25209ae3e0ab
karadz <karadz@gmail.com>
2022-03-30 09:17:22 +0200

    Corrected contact email to use sparse

--- src/routes/api/contacts/upload.json.ts
+++ src/routes/api/contacts/upload.json.ts
@@ -2,62 +2,68 @@ import parseCSV from '$lib/services/csvParse.services';
 import { postSuite } from '$lib/validation/server/contacts.validate';
 import ContactsModel from '$lib/models/contacts.model';
 
-import { omitBy } from 'lodash';
+import { pickBy, identity } from 'lodash';
 
 import type { RequestHandler } from '@sveltejs/kit';
 
 export const post: RequestHandler = async ({ request }) => {
-	const jsonFile = await parseCSV(request);
+	try {
+		const jsonFile = await parseCSV(request);
 
-	/**
-	 * name
-	 * phone
-	 * isActive: true
-	 */
-	const finalContact = [];
+		/**
+		 * name
+		 * phone
+		 * isActive: true
+		 */
+		// const finalContact = [];
 
-	interface contactInterface {
-		Name: string;
-		Email: string;
-		Phone: string;
-		Organization: string;
-		isActive: string;
+		interface contactInterface {
+			Name: string;
+			Email: string;
+			Phone: string;
+			Organization: string;
+			isActive: string;
+		}
+		jsonFile.forEach(async (element) => {
+			const { Name, Email, Phone }: contactInterface = element;
+
+			const name = Name.replace(/Emb$/gm, '').trim();
+			const email = Email.trim();
+			const phone = Phone.split(',')[0].trim().replace(/ /g, '');
+
+			const contact = { name, email, phone, isActive: true, isUser: false };
+
+			const contactFiltered = pickBy(contact, identity);
+
+			const result = postSuite(contactFiltered);
+
+			if (result.hasErrors()) {
+				return {
+					status: 400,
+					body: {
+						message: result.getErrors()
+					}
+				};
+			}
+
+			const newContacts = new ContactsModel(contactFiltered);
+
+			await newContacts.save();
+		});
+
+		return {
+			status: 200,
+			body: {
+				message: 'ok'
+			}
+		};
+	} catch (err) {
+		console.error('Error', err.message);
+		return {
+			status: 500,
+			body: {
+				message: 'Error has ocurred'
+			}
+		};
 	}
-	jsonFile.forEach(async (element) => {
-		const { Name, Email, Phone }: contactInterface = element;
-
-		const name = Name.replace(/Emb$/gm, '').trim();
-		const email = Email.trim();
-		const phone = Phone.split(',')[0].trim().replace(/ /g, '');
-
-		const contact = { name, email, phone, isActive: true };
-
-		const contactFiltered = omitBy(contact, (v) => v === '');
-		// finalContact.push(contactFiltered);
-
-		const result = postSuite(contactFiltered);
-
-		if (result.hasErrors()) {
-			// console.log('Entered Errors', result.getErrors());
-			return {
-				status: 400,
-				body: {
-					message: result.getErrors()
-				}
-			};
-		}
-
-		const newContacts = new ContactsModel(contactFiltered);
-
-		// console.log('newContacts', newContacts);
-
-		await newContacts.save();
-	});
-
-	return {
-		status: 200,
-		body: {
-			message: 'ok'
-		}
-	};
 };
