6803f4d27197f1ac96da095d0746aa4beaacdac8
bkaradz <karadz@gmail.com>
2022-03-31 17:45:21 +0200

    Working on contacts query frontend

--- src/lib/services/query.services.ts
+++ src/lib/services/query.services.ts
@@ -1,52 +1,46 @@
-import omit from 'lodash/omit';
+import omit from 'lodash/omit'
 
 const query = async (query, model) => {
-	try {
-		const results = {};
-		let { limit, page } = query;
+  try {
+    const results = {}
+    let { limit, page } = query
 
-		limit = parseInt(limit);
-		page = parseInt(page);
+    limit = parseInt(limit)
+    page = parseInt(page)
 
-		const newQuery = omit(query, ['limit', 'page']);
+    const newQuery = omit(query, ['limit', 'page'])
 
-		const startIndex = (page - 1) * limit;
-		const endIndex = page * limit;
+    const startIndex = (page - 1) * limit
+    const endIndex = page * limit
 
-		if (startIndex > 0) {
-			results.previous = {
-				page: page - 1,
-				limit
-			};
-		}
-		if (endIndex < (await await model.countDocuments({ ...newQuery }).exec())) {
-			results.next = {
-				page: page + 1,
-				limit
-			};
-		}
+    const totalRecords = await await model.countDocuments({ ...newQuery }).exec()
 
-		results.results = await model
-			.find({ ...newQuery })
-			.select('-password')
-			.limit(limit)
-			.skip(startIndex)
-			.exec();
+    results.totalRecords = totalRecords
 
-		return {
-			status: 200,
-			body: {
-				message: results
-			}
-		};
-	} catch (err) {
-		return {
-			status: 500,
-			body: {
-				message: err.message
-			}
-		};
-	}
-};
+    if (startIndex > 0) {
+      results.previous = {
+        page: page - 1,
+        limit,
+      }
+    }
+    if (endIndex < totalRecords) {
+      results.next = {
+        page: page + 1,
+        limit,
+      }
+    }
 
-export default query;
+    results.results = await model
+      .find({ ...newQuery })
+      .select('-password')
+      .limit(limit)
+      .skip(startIndex)
+      .exec()
+
+    return results
+  } catch (err) {
+    return err.message
+  }
+}
+
+export default query
--- src/routes/contacts/index.svelte
+++ src/routes/contacts/index.svelte
@@ -2,9 +2,15 @@
 	import type { Load } from '@sveltejs/kit';
 
 	export const load = async ({ fetch }) => {
-		const res = await fetch('/api/contacts.json');
+		let paramsObj = {
+			limit: 15,
+			page: 1
+		};
+		let searchParams = new URLSearchParams(paramsObj);
+		const res = await fetch('/api/contacts.json?' + searchParams.toString());
 		if (res.ok) {
 			const contacts = await res.json();
+			console.log('contacts', contacts);
 			return {
 				props: { contacts }
 			};
@@ -30,6 +36,8 @@
 	import { contactsList } from '$lib/stores/contactsTempList';
 	import { goto } from '$app/navigation';
 	export let contacts;
+
+	console.log('contacts front', contacts.results);
 
 	let noContactsPerPage = 10;
 	let paginationCurrentValue = 2;
@@ -248,7 +256,7 @@
 	</div>
 	<!-- List of Contacts -->
 	<div class="mt-6 flex flex-1 flex-wrap gap-4 overflow-y-auto">
-		{#each contacts.contacts as contact (contact._id)}
+		{#each contacts.results as contact (contact._id)}
 			<div
 				on:click={viewContact(contact._id)}
 				class=" flex h-44 w-full max-w-xs grow flex-col border-t-4 border-royal-blue-500 bg-white shadow-lg hover:cursor-pointer hover:bg-royal-blue-200 lg:w-1/6"
