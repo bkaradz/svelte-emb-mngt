62291984af9cc335fb8cdb02fb231f62d31993e7
bkaradz <karadz@gmail.com>
2022-04-12 17:09:49 +0200

    Finished the headlessUI list

--- src/lib/utility/svgLogos.ts
+++ src/lib/utility/svgLogos.ts
@@ -10,7 +10,7 @@ export const svgLogout = `<svg
 								stroke-width="2"
 								d="M17 16l4-4m0 0l-4-4m4 4H7m6 4v1a3 3 0 01-3 3H6a3 3 0 01-3-3V7a3 3 0 013-3h4a3 3 0 013 3v1"
 							/></svg
-						>`;
+						>`
 
 export const svgSettings = `<svg
 							class="h-6 w-6"
@@ -29,7 +29,7 @@ export const svgSettings = `<svg
 								stroke-width="2"
 								d="M15 12a3 3 0 11-6 0 3 3 0 016 0z"
 							/></svg
-						>`;
+						>`
 
 export const svgUser = `<svg
 							class="h-6 w-6"
@@ -43,7 +43,7 @@ export const svgUser = `<svg
 								stroke-width="2"
 								d="M16 7a4 4 0 11-8 0 4 4 0 018 0zM12 14a7 7 0 00-7 7h14a7 7 0 00-7-7z"
 							/></svg
-						>`;
+						>`
 
 export const svgMessages = `<svg
 			class="h-7 w-7 fill-royal-blue-300 stroke-royal-blue-600"
@@ -57,7 +57,7 @@ export const svgMessages = `<svg
 				stroke-width="2"
 				d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9"
 			/></svg
-		>`;
+		>`
 
 export const svgMenu = `<svg
 				class="h-7 w-7 text-royal-blue-600 hover:text-royal-blue-700"
@@ -71,7 +71,7 @@ export const svgMenu = `<svg
 					d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z"
 					clip-rule="evenodd"
 				/>
-			</svg>`;
+			</svg>`
 
 export const svgArrow = `<svg
 				class="h-7 w-7 text-royal-blue-600 hover:text-royal-blue-700"
@@ -85,7 +85,7 @@ export const svgArrow = `<svg
 					stroke-width="2"
 					d="M10 19l-7-7m0 0l7-7m-7 7h18"
 				/></svg
-			>`;
+			>`
 
 export const svgPlus = `<svg
 					xmlns="http://www.w3.org/2000/svg"
@@ -100,7 +100,7 @@ export const svgPlus = `<svg
 						stroke-width="2"
 						d="M12 6v6m0 0v6m0-6h6m-6 0H6"
 					/>
-				</svg>`;
+				</svg>`
 
 export const svgSearch = `<svg
 							class="h-4 w-4 text-pickled-bluewood-600"
@@ -113,7 +113,7 @@ export const svgSearch = `<svg
 							<path
 								d="M16.32 14.9l5.39 5.4a1 1 0 0 1-1.42 1.4l-5.38-5.38a8 8 0 1 1 1.41-1.41zM10 16a6 6 0 1 0 0-12 6 6 0 0 0 0 12z"
 							/>
-						</svg>`;
+						</svg>`
 
 export const svgSelector = `<svg
 								class="text-royal-royal-blue-600 ml-2 mr-3 h-6 w-6"
@@ -129,7 +129,7 @@ export const svgSelector = `<svg
 									stroke-linejoin="round"
 									d="M8 9l4-4 4 4m0 6l-4 4-4-4"
 								/>
-							</svg>`;
+							</svg>`
 
 export const svgSort = `<svg
 								xmlns="http://www.w3.org/2000/svg"
@@ -137,7 +137,7 @@ export const svgSort = `<svg
 								width="24"
 								height="24"
 								viewBox="0 0 24 24"><path d="M3 18h6v-2H3v2zM3 6v2h18V6H3zm0 7h12v-2H3v2z" /></svg
-							>`;
+							>`
 
 export const svgList = `<svg
 					class="h-9 w-9"
@@ -156,7 +156,7 @@ export const svgList = `<svg
 						d="M17 7V9H11V7H17Z"
 						fill="currentColor"
 					/></svg
-				>`;
+				>`
 
 export const svgGrid = `<svg
 					class="h-9 w-9"
@@ -178,26 +178,31 @@ export const svgGrid = `<svg
 						d="M11 15H13V17H11V15Z"
 						fill="currentColor"
 					/><path d="M17 15H15V17H17V15Z" fill="currentColor" /></svg
-				>`;
+				>`
 
 export const svgChevronLeft = `<svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M12.707 5.293a1 1 0 010 1.414L9.414 10l3.293 3.293a1 1 0 01-1.414 1.414l-4-4a1 1 0 010-1.414l4-4a1 1 0 011.414 0z" clip-rule="evenodd" />
-          </svg>`;
+          </svg>`
 
 export const svgChevronRight = `<svg class="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
             <path fill-rule="evenodd" d="M7.293 14.707a1 1 0 010-1.414L10.586 10 7.293 6.707a1 1 0 011.414-1.414l4 4a1 1 0 010 1.414l-4 4a1 1 0 01-1.414 0z" clip-rule="evenodd" />
-          </svg>`;
+          </svg>`
 
 export const svgView = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
   <path stroke-linecap="round" stroke-linejoin="round" d="M15 12a3 3 0 11-6 0 3 3 0 016 0z" />
   <path stroke-linecap="round" stroke-linejoin="round" d="M2.458 12C3.732 7.943 7.523 5 12 5c4.478 0 8.268 2.943 9.542 7-1.274 4.057-5.064 7-9.542 7-4.477 0-8.268-2.943-9.542-7z" />
-</svg>`;
+</svg>`
 
 export const svgDotsVertical = `<svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
   <path d="M10 6a2 2 0 110-4 2 2 0 010 4zM10 12a2 2 0 110-4 2 2 0 010 4zM10 18a2 2 0 110-4 2 2 0 010 4z"/>
 </svg>
-`;
+`
+
 export const svgUpload = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
   <path fill-rule="evenodd" d="M3 17a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM6.293 6.707a1 1 0 010-1.414l3-3a1 1 0 011.414 0l3 3a1 1 0 01-1.414 1.414L11 5.414V13a1 1 0 11-2 0V5.414L7.707 6.707a1 1 0 01-1.414 0z" clip-rule="evenodd"/>
 </svg>
-`;
+`
+
+export const svgCheck = `<svg xmlns="http://www.w3.org/2000/svg" class="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+  <path stroke-linecap="round" stroke-linejoin="round" d="M5 13l4 4L19 7" />
+</svg>`
--- src/routes/test.svelte
+++ src/routes/test.svelte
@@ -1,169 +1,166 @@
-<script>
-	import { svgSelector, svgSort, svgDotsVertical } from '$lib/utility/svgLogos';
-	import { Menu, MenuItems, MenuItem, MenuButton } from '@rgossiaux/svelte-headlessui';
+<script lang="ts">
+	import { svgCheck, svgSelector } from '$lib/utility/svgLogos';
+
+	let selectedValue: string = 'Javascript';
+	const changeSelection = (e) => {
+		console.log('e.target.value', e.target.name);
+		selectedValue = e.target.name;
+		// selectedValue = selectedValue;
+	};
+	$: console.log('selectedValue', selectedValue);
+
+	import {
+		Listbox,
+		ListboxButton,
+		ListboxOptions,
+		ListboxOption
+	} from '@rgossiaux/svelte-headlessui';
+
+	const people = [
+		{ id: 1, name: 'Name', value: 'name', unavailable: false },
+		{ id: 2, name: 'Organistion', unavailable: false },
+		{ id: 3, name: 'Phone', unavailable: false },
+		{ id: 4, name: 'Email', unavailable: false },
+		{ id: 5, name: 'Vat Number', unavailable: false },
+		{ id: 6, name: 'Balance Due', unavailable: false },
+		{ id: 7, name: 'State', unavailable: false }
+	];
+
+	let selectedPerson = people[0];
+	$: console.log('selectedPerson', selectedPerson);
 </script>
 
-<h1>Test</h1>
+<div class="min-h-screen bg-pickled-bluewood-100 p-10">
+	<div class="mx-auto max-w-md">
+		<label for="select" class="block py-2 font-semibold">Select Input:</label>
 
-<Menu as="div" class="relative">
-	<MenuButton
-		class="btn inline-flex w-full items-center justify-center px-2 py-2 text-sm text-pickled-bluewood-500 hover:bg-pickled-bluewood-50 focus:outline-none focus:ring-2 focus:ring-royal-blue-50 focus:ring-offset-2 focus:ring-offset-pickled-bluewood-100"
-		id="menu-button"
-		aria-expanded="true"
-		aria-haspopup="true"
-	>
-		<span>
-			{@html svgSort}
-		</span>
+		<div class="relative">
+			<div class="flex h-10 items-center rounded border border-pickled-bluewood-200 bg-white">
+				<input
+					bind:value={selectedValue}
+					name="select"
+					id="select"
+					class="w-full appearance-none px-4 text-pickled-bluewood-800 outline-none"
+					checked
+				/>
 
-		Sort by Name
-		<span>
-			{@html svgSelector}
-		</span>
-	</MenuButton>
-
-	<MenuItems
-		class=" absolute left-0 top-9 z-10 mt-2 w-40 origin-top-right divide-y divide-pickled-bluewood-100 bg-white shadow-lg ring-1 ring-royal-blue-300 focus:outline-none"
-		role="menu"
-		aria-orientation="vertical"
-		aria-labelledby="menu-button"
-		tabindex="-1"
-	>
-		<div class="py-1" role="none">
-			<MenuItem active={true}>
-				<a
-					href="/"
-					class="block px-4 py-2 text-sm text-pickled-bluewood-700 hover:bg-royal-blue-500 hover:text-white"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-0">Edit</a
+				<!-- <button
+					class="cursor-pointer text-pickled-bluewood-300 outline-none transition-all hover:text-pickled-bluewood-600 focus:outline-none"
 				>
-			</MenuItem>
-			<MenuItem>
-				<a
-					href="/"
-					class="block px-4 py-2 text-sm text-pickled-bluewood-700 hover:bg-royal-blue-500 hover:text-white"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-1">Duplicate</a
+					<svg
+						class="mx-2 h-4 w-4 fill-current"
+						xmlns="http://www.w3.org/2000/svg"
+						viewBox="0 0 24 24"
+						stroke="currentColor"
+						stroke-width="2"
+						stroke-linecap="round"
+						stroke-linejoin="round"
+					>
+						<line x1="18" y1="6" x2="6" y2="18" />
+						<line x1="6" y1="6" x2="18" y2="18" />
+					</svg>
+				</button> -->
+				<label
+					for="show_more"
+					class="cursor-pointer border-l border-pickled-bluewood-200 text-pickled-bluewood-300 outline-none transition-all hover:text-pickled-bluewood-600 focus:outline-none"
 				>
-			</MenuItem>
+					{@html svgSelector}
+				</label>
+			</div>
+
+			<input type="checkbox" name="show_more" id="show_more" class="peer hidden" checked />
+			<div
+				class="absolute mt-1 hidden w-full flex-col overflow-hidden rounded border border-pickled-bluewood-200 bg-white shadow peer-checked:flex"
+			>
+				<div class="group cursor-pointer">
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<a
+						on:click={changeSelection}
+						name="Python"
+						class="{selectedValue === 'Python'
+							? 'border-royal-blue-600'
+							: ''} block border-l-4 border-transparent p-2 group-hover:border-royal-blue-600 group-hover:bg-pickled-bluewood-100"
+						>Python</a
+					>
+				</div>
+				<div class="group cursor-pointer border-t border-pickled-bluewood-200">
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<a
+						on:click={changeSelection}
+						name="Javascript"
+						class="{selectedValue === 'Javascript'
+							? 'border-royal-blue-600'
+							: ''} border-blue-600 block border-l-4 border-transparent p-2 group-hover:border-royal-blue-600 group-hover:bg-pickled-bluewood-100"
+						>Javascript</a
+					>
+				</div>
+				<div class="group cursor-pointer border-t border-pickled-bluewood-200">
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<a
+						on:click={changeSelection}
+						name="Node"
+						class="{selectedValue === 'Node'
+							? 'border-royal-blue-600'
+							: ''} block border-l-4 border-transparent p-2 group-hover:border-royal-blue-600 group-hover:bg-pickled-bluewood-100"
+						>Node</a
+					>
+				</div>
+				<div class="group cursor-pointer border-t border-pickled-bluewood-200">
+					<!-- svelte-ignore a11y-missing-attribute -->
+					<a
+						on:click={changeSelection}
+						name="PHP"
+						class="{selectedValue === 'PHP'
+							? 'border-royal-blue-600'
+							: ''} block border-l-4 border-transparent p-2 group-hover:border-royal-blue-600 group-hover:bg-pickled-bluewood-100"
+						>PHP</a
+					>
+				</div>
+			</div>
 		</div>
-		<div class="py-1" role="none">
-			<MenuItem>
-				<a
-					href="/"
-					class="block px-4 py-2 text-sm text-pickled-bluewood-700 hover:bg-royal-blue-500 hover:text-white"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-2">Archive</a
-				>
-			</MenuItem>
-			<MenuItem>
-				<a
-					href="/"
-					class="block px-4 py-2 text-sm text-pickled-bluewood-700 hover:bg-royal-blue-500 hover:text-white"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-3">Move</a
-				>
-			</MenuItem>
-		</div>
-		<div class="py-1" role="none">
-			<MenuItem>
-				<a
-					href="/"
-					class="block px-4 py-2 text-sm text-pickled-bluewood-700 hover:bg-royal-blue-500 hover:text-white"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-4">Share</a
-				>
-			</MenuItem>
-			<MenuItem>
-				<a
-					href="/"
-					class="block px-4 py-2 text-sm text-pickled-bluewood-700 hover:bg-royal-blue-500 hover:text-white"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-5">Add to favorites</a
-				>
-			</MenuItem>
-		</div>
-		<div class="py-1" role="none">
-			<MenuItem>
-				<a
-					href="/"
-					class="block px-4 py-2 text-sm text-pickled-bluewood-700 hover:bg-royal-blue-500 hover:text-white"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-6">Delete</a
-				>
-			</MenuItem>
-		</div>
-	</MenuItems>
-</Menu>
+	</div>
+</div>
 
-<Menu as="div" class="relative">
-	<MenuButton
-		class="btn inline-flex w-full items-center justify-center px-2 py-2 text-sm text-pickled-bluewood-500 hover:bg-pickled-bluewood-50 focus:outline-none focus:ring-2 focus:ring-royal-blue-50 focus:ring-offset-2 focus:ring-offset-pickled-bluewood-100"
-		id="menu-button"
-		aria-expanded="true"
-		aria-haspopup="true"
-	>
-		<span>
-			{@html svgDotsVertical}
-		</span>
-	</MenuButton>
-
-	<MenuItems
-		class=" absolute right-0 top-9 z-10 mt-2 w-20 origin-top-right divide-y divide-pickled-bluewood-100 bg-white shadow-lg ring-1 ring-royal-blue-300 focus:outline-none"
-		role="menu"
-		aria-orientation="vertical"
-		aria-labelledby="menu-button"
-		tabindex="-1"
-	>
-		<div class="py-1" role="none">
-			<MenuItem active={true}>
-				<a
-					href="/"
-					class="block px-4 py-2 text-sm text-pickled-bluewood-700 hover:bg-royal-blue-500 hover:text-white"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-0">Edit</a
+<div class=" w-[168px]">
+	<Listbox class="" value={selectedPerson} on:change={(e) => (selectedPerson = e.detail)}>
+		<ListboxButton
+			class="focus-visible:ring-offset-orange-300 relative w-full cursor-default bg-white py-2 pl-3 pr-3 text-left shadow-md focus:outline-none focus-visible:border-royal-blue-500 focus-visible:ring-2 focus-visible:ring-white focus-visible:ring-opacity-75 focus-visible:ring-offset-2 sm:text-sm"
+		>
+			<span class="pointer-events-none absolute inset-y-0 left-0 flex items-center">
+				{@html svgSelector}
+			</span>
+			<span class="ml-6 block truncate text-right text-xs">
+				Search By {selectedPerson.name}
+			</span>
+		</ListboxButton>
+		<ListboxOptions
+			class="ring-black absolute mt-1 w-40 overflow-auto bg-white py-1 text-base shadow-lg ring-1 ring-opacity-5 focus:outline-none sm:text-sm"
+		>
+			{#each people as person (person.id)}
+				<!-- Use the `active` state to conditionally style the active (focused) option -->
+				<!-- Use the `selected` state to conditionally style the selected option -->
+				<ListboxOption
+					value={person}
+					disabled={person.unavailable}
+					class={({ active }) =>
+						`cursor-default select-none relative py-2 pl-10 pr-4 ${
+							active
+								? 'text-pickled-bluewood-900 bg-pickled-bluewood-100'
+								: 'text-pickled-bluewood-900'
+						}`}
+					let:selected
+					let:active
 				>
-			</MenuItem>
-			<MenuItem>
-				<a
-					href="/"
-					class="block px-4 py-2 text-sm text-pickled-bluewood-700 hover:bg-royal-blue-500 hover:text-white"
-					role="menuitem"
-					tabindex="-1"
-					id="menu-item-1">Delete</a
-				>
-			</MenuItem>
-		</div>
-	</MenuItems>
-</Menu>
-
-<input
-	class=" file:btn-primary file:border-none file:bg-royal-blue-600 file:text-white"
-	type="file"
-	name="Upload"
-	id=""
-/>
-
-<div class="relative mt-4 mb-4 overflow-hidden">
-	<input
-		class="btn btn-primary absolute block cursor-pointer opacity-0"
-		type="file"
-		name="vacancyImageFiles"
-	/>
-	<button
-		class="inline-flex w-full items-center bg-royal-blue-500 py-2 px-2 font-bold text-white hover:bg-royal-blue-600"
-	>
-		<svg fill="#FFF" height="18" viewBox="0 0 24 24" width="18" xmlns="http://www.w3.org/2000/svg">
-			<path d="M0 0h24v24H0z" fill="none" />
-			<path d="M9 16h6v-6h4l-7-7-7 7h4zm-4 2h14v2H5z" />
-		</svg>
-	</button>
+					<span class="absolute inset-y-0 left-0 flex items-center pl-3 text-pickled-bluewood-600">
+						{#if selected}
+							{@html svgCheck}
+						{/if}
+					</span>
+					<span>
+						{person.name}
+					</span>
+				</ListboxOption>
+			{/each}
+		</ListboxOptions>
+	</Listbox>
 </div>
