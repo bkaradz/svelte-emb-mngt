2a791f3a659cfdbbacbd6a83732fe56af0292c58
bkaradz <karadz@gmail.com>
2022-06-06 16:47:57 +0200

    Added company profile on settings

--- src/routes/production/index.svelte
+++ src/routes/production/index.svelte
@@ -1,25 +1,9 @@
 <script lang="ts">
-	const heandleSubmit = async (e: SubmitEvent) => {
-		const formElm = e.target as HTMLFormElement;
-		const formData = new FormData(formElm);
-
-		const res = await fetch('/api/test.json', {
-			method: 'POST',
-			body: formData
-		});
-
-		if (res.ok) {
-			formElm.reset();
-		}
-	};
 </script>
 
-<h1>Production</h1>
-
-<form on:submit|preventDefault={heandleSubmit}>
-	<input class="btn btn-primary" type="file" name="file" accept=".csv" />
-	<button class="btn btn-primary" type="submit">Submit</button>
-</form>
+<div>
+	<h1>Production</h1>
+</div>
 
 <style>
 </style>
