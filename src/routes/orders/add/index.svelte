7e264c0efe01b4559c69c46c8ce1e97fc33d5f5c
bkaradz <karadz@gmail.com>
2022-06-30 16:46:26 +0200

    feat(orders): working on the orders UI

--- src/routes/orders/add/index.svelte
+++ src/routes/orders/add/index.svelte
@@ -1,10 +1,27 @@
 <script lang="ts">
+	import { goto } from '$app/navigation';
 	import AddOrder from '$lib/components/orders/addOrder.svelte';
+	import { svgArrow } from '$lib/utility/svgLogos';
+
+	const gotoOrders = async () => {
+		await goto(`/orders`);
+	};
 </script>
 
 <AddOrder>
 	<div slot="header">
-		<h1 class=" text-2xl">Header</h1>
+		<div class="flex justify-between">
+			<div class="flex">
+				<button class="mr-3" on:click={gotoOrders}>
+					{@html svgArrow}
+				</button>
+				<h1 class="text-slate-700 text-2xl font-medium">New Quotation</h1>
+			</div>
+			<div>
+				<button class="bg-royal-blue-500  px-5 py-2 rounded text-white mr-4">Cancel</button>
+				<button class="bg-royal-blue-500  px-5 py-2 rounded text-white" type="submit">Done</button>
+			</div>
+		</div>
 	</div>
 	<div slot="page">
 		<h1>Page</h1>
